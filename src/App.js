56408cf4697fe3ac2e4505c179e079690953d6f4
philliphall131 <phillip.hall131@gmail.com>
2022-04-09 15:10:51 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import About from './pages/About.js'
+import HomePage from './pages/HomePage.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        {/* <nav>
+            <ul id="navigation">
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+            </ul>
+        </nav> */}
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Router>
     </div>
   );
 }
---
+++ src/pages/HomePage.js
@@ -0,0 +1,19 @@
+import '../App.css';
+import React from 'react';
+import logo from '../logo.svg';
+
+class HomePage extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                <h1>Hello Ivan</h1>
+                </header>
+            </div>
+            
+        );
+    }
+
+}
+
+export default HomePage;
